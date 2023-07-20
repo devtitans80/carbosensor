3c5a28296c7fef04587cf86e1790a485350782e0
Charles Araujo <callmatos@gmail.com>
2023-07-19 20:38:44 -0400

    update kernel to smartcarbo

--- device/devtitans/kernel_carbosensor/smartcarbo-module/smartcarbo.c
+++ device/devtitans/kernel_carbosensor/smartcarbo-module/smartcarbo.c
@@ -3,7 +3,7 @@
 #include <linux/slab.h>
 
 MODULE_AUTHOR("DevTITANS <devtitans@icomp.ufam.edu.br>");
-MODULE_DESCRIPTION("Driver de acesso ao SmartLamp (ESP32 com Chip Serial CP2102");
+MODULE_DESCRIPTION("Driver de acesso ao SmartCarbo (ESP32 com Chip Serial CP2102");
 MODULE_LICENSE("GPL");
 
 // Tamanho máximo de uma linha de resposta do dispositvo USB
@@ -12,22 +12,21 @@ MODULE_LICENSE("GPL");
 static int  usb_probe(struct usb_interface *ifce, const struct usb_device_id *id); // Executado quando o dispositivo é conectado na USB
 static void usb_disconnect(struct usb_interface *ifce);                            // Executado quando o dispositivo USB é desconectado da USB
 static int  usb_send_cmd(char *cmd, int param);                                    // Envia um comando via USB e espera/retorna a resposta do dispositivo (int)
-// Executado quando o arquivo /sys/kernel/smartlamp/{led, ldr, threshold} é lido (e.g., cat /sys/kernel/smartlamp/led)
+// Executado quando o arquivo /sys/kernel/smartcarbosensor/{led, ldr} é lido (e.g., cat /sys/kernel/smartcarbosensor/led)
 static ssize_t attr_show(struct kobject *sys_obj, struct kobj_attribute *attr, char *buff);
-// Executado quando o arquivo /sys/kernel/smartlamp/{led, ldr, threshold} é escrito (e.g., echo "100" | sudo tee -a /sys/kernel/smartlamp/led)
+// Executado quando o arquivo /sys/kernel/smartcarbosensor/{led, ldr} é escrito (e.g., echo "100" | sudo tee -a /sys/kernel/smartcarbosensor/led)
 static ssize_t attr_store(struct kobject *sys_obj, struct kobj_attribute *attr, const char *buff, size_t count);
 
 static char recv_line[MAX_RECV_LINE];              // Armazena dados vindos da USB até receber um caractere de nova linha '\n'
-static struct usb_device *smartlamp_device;        // Referência para o dispositivo USB
+static struct usb_device *smartcarbosensor_device;        // Referência para o dispositivo USB
 static uint usb_in, usb_out;                       // Endereços das portas de entrada e saida da USB
 static char *usb_in_buffer, *usb_out_buffer;       // Buffers de entrada e saída da USB
 static int usb_max_size;                           // Tamanho máximo de uma mensagem USB
 
-// Variáveis para criar os arquivos no /sys/kernel/smartlamp/{led, ldr, threshold}
+// Variáveis para criar os arquivos no /sys/kernel/smartcarbosensor/{led, ldr}
 static struct kobj_attribute  led_attribute = __ATTR(led, S_IRUGO | S_IWUSR, attr_show, attr_store);
 static struct kobj_attribute  ldr_attribute = __ATTR(ldr, S_IRUGO | S_IWUSR, attr_show, attr_store);
-static struct kobj_attribute  threshold_attribute = __ATTR(threshold, S_IRUGO | S_IWUSR, attr_show, attr_store);
-static struct attribute      *attrs[]       = { &led_attribute.attr, &ldr_attribute.attr, &threshold_attribute.attr, NULL };
+static struct attribute      *attrs[]       = { &led_attribute.attr, &ldr_attribute.attr, NULL };
 static struct attribute_group attr_group    = { .attrs = attrs };
 static struct kobject        *sys_obj;
 
@@ -37,27 +36,27 @@ static struct kobject        *sys_obj;
 static const struct usb_device_id id_table[] = { { USB_DEVICE(VENDOR_ID, PRODUCT_ID) }, {} };
 MODULE_DEVICE_TABLE(usb, id_table);
 
-// Cria e registra o driver do smartlamp no kernel
-static struct usb_driver smartlamp_driver = {
-    .name        = "smartlamp",     // Nome do driver
+// Cria e registra o driver do smartcarbosensor no kernel
+static struct usb_driver smartcarbosensor_driver = {
+    .name        = "smartcarbosensor",     // Nome do driver
     .probe       = usb_probe,       // Executado quando o dispositivo é conectado na USB
     .disconnect  = usb_disconnect,  // Executado quando o dispositivo é desconectado na USB
     .id_table    = id_table,        // Tabela com o VendorID e ProductID do dispositivo
 };
-module_usb_driver(smartlamp_driver);
+module_usb_driver(smartcarbosensor_driver);
 
 // Executado quando o dispositivo é conectado na USB
 static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id) {
     struct usb_endpoint_descriptor *usb_endpoint_in, *usb_endpoint_out;
 
-    printk(KERN_INFO "SmartLamp: Dispositivo conectado ...\n");
+    printk(KERN_INFO "SmartCarbo: Dispositivo conectado ...\n");
 
-    // Cria arquivos do /sys/kernel/smartlamp/*
-    sys_obj = kobject_create_and_add("smartlamp", kernel_kobj);
+    // Cria arquivos do /sys/kernel/smartcarbosensor/*
+    sys_obj = kobject_create_and_add("smartcarbosensor", kernel_kobj);
     sysfs_create_group(sys_obj, &attr_group);
 
     // Detecta portas e aloca buffers de entrada e saída de dados na USB
-    smartlamp_device = interface_to_usbdev(interface);
+    smartcarbosensor_device = interface_to_usbdev(interface);
     usb_find_common_endpoints(interface->cur_altsetting, &usb_endpoint_in, &usb_endpoint_out, NULL, NULL);
     usb_max_size = usb_endpoint_maxp(usb_endpoint_in);
     usb_in = usb_endpoint_in->bEndpointAddress;
@@ -71,7 +70,7 @@ static int usb_probe(struct usb_interface *interface, const struct usb_device_id
 // Executado quando o dispositivo USB é desconectado da USB
 static void usb_disconnect(struct usb_interface *interface) {
     printk(KERN_INFO "SmartLamp: Dispositivo desconectado.\n");
-    if (sys_obj) kobject_put(sys_obj);      // Remove os arquivos em /sys/kernel/smartlamp
+    if (sys_obj) kobject_put(sys_obj);      // Remove os arquivos em /sys/kernel/smartcarbosensor
     kfree(usb_in_buffer);                   // Desaloca buffers
     kfree(usb_out_buffer);
 }
@@ -87,15 +86,15 @@ static int usb_send_cmd(char *cmd, int param) {
     char *resp_pos;                         // Posição na linha lida que contém o número retornado pelo dispositivo
     long resp_number = -1;                  // Número retornado pelo dispositivo (e.g., valor do led, valor do ldr)
 
-    printk(KERN_INFO "SmartLamp: Enviando comando: %s\n", cmd);
+    printk(KERN_INFO "SmartCarbo: Enviando comando: %s\n", cmd);
 
     if (param >= 0) sprintf(usb_out_buffer, "%s %d\n", cmd, param); // Se param >=0, o comando possui um parâmetro (int)
     else sprintf(usb_out_buffer, "%s\n", cmd);                      // Caso contrário, é só o comando mesmo
 
     // Envia o comando (usb_out_buffer) para a USB
-    ret = usb_bulk_msg(smartlamp_device, usb_sndbulkpipe(smartlamp_device, usb_out), usb_out_buffer, strlen(usb_out_buffer), &actual_size, 1000*HZ);
+    ret = usb_bulk_msg(smartcarbosensor_device, usb_sndbulkpipe(smartcarbosensor_device, usb_out), usb_out_buffer, strlen(usb_out_buffer), &actual_size, 1000*HZ);
     if (ret) {
-        printk(KERN_ERR "SmartLamp: Erro de codigo %d ao enviar comando!\n", ret);
+        printk(KERN_ERR "SmartCarbo: Erro de codigo %d ao enviar comando!\n", ret);
         return -1;
     }
 
@@ -104,9 +103,9 @@ static int usb_send_cmd(char *cmd, int param) {
     // Espera pela resposta correta do dispositivo (desiste depois de várias tentativas)
     while (retries > 0) {
         // Lê dados da USB
-        ret = usb_bulk_msg(smartlamp_device, usb_rcvbulkpipe(smartlamp_device, usb_in), usb_in_buffer, min(usb_max_size, MAX_RECV_LINE), &actual_size, HZ*1000);
+        ret = usb_bulk_msg(smartcarbosensor_device, usb_rcvbulkpipe(smartcarbosensor_device, usb_in), usb_in_buffer, min(usb_max_size, MAX_RECV_LINE), &actual_size, HZ*1000);
         if (ret) {
-            printk(KERN_ERR "SmartLamp: Erro ao ler dados da USB (tentativa %d). Codigo: %d\n", ret, retries--);
+            printk(KERN_ERR "SmartCarbo: Erro ao ler dados da USB (tentativa %d). Codigo: %d\n", ret, retries--);
             continue;
         }
 
@@ -115,11 +114,11 @@ static int usb_send_cmd(char *cmd, int param) {
 
             if (usb_in_buffer[i] == '\n') {  // Temos uma linha completa
                 recv_line[recv_size] = '\0';
-                printk(KERN_INFO "SmartLamp: Recebido uma linha: '%s'\n", recv_line);
+                printk(KERN_INFO "SmartCarbo: Recebido uma linha: '%s'\n", recv_line);
 
                 // Verifica se o início da linha recebida é igual à resposta esperada do comando enviado
                 if (!strncmp(recv_line, resp_expected, strlen(resp_expected))) {
-                    printk(KERN_INFO "SmartLamp: Linha eh resposta para %s! Processando ...\n", cmd);
+                    printk(KERN_INFO "SmartCarbo: Linha eh resposta para %s! Processando ...\n", cmd);
 
                     // Acessa a parte da resposta que contém o número e converte para inteiro
                     resp_pos = &recv_line[strlen(resp_expected) + 1];
@@ -128,7 +127,7 @@ static int usb_send_cmd(char *cmd, int param) {
                     return resp_number;
                 }
                 else { // Não é a linha que estávamos esperando. Pega a próxima.
-                    printk(KERN_INFO "SmartLamp: Nao eh resposta para %s! Tentiva %d. Proxima linha...\n", cmd, retries--);
+                    printk(KERN_INFO "SmartCarbo: Nao eh resposta para %s! Tentiva %d. Proxima linha...\n", cmd, retries--);
                     recv_size = 0; // Limpa a linha lida (recv_line)
                 }
             }
@@ -141,47 +140,43 @@ static int usb_send_cmd(char *cmd, int param) {
     return -1; // Não recebi a resposta esperada do dispositivo
 }
 
-// Executado quando o arquivo /sys/kernel/smartlamp/{led, ldr, threshold} é lido (e.g., cat /sys/kernel/smartlamp/led)
+// Executado quando o arquivo /sys/kernel/smartcarbosensor/{led, ldr} é lido (e.g., cat /sys/kernel/smartcarbosensor/led)
 static ssize_t attr_show(struct kobject *sys_obj, struct kobj_attribute *attr, char *buff) {
     int value;
     const char *attr_name = attr->attr.name;
 
-    printk(KERN_INFO "SmartLamp: Lendo %s ...\n", attr_name);
+    printk(KERN_INFO "SmartCarbo: Lendo %s ...\n", attr_name);
 
     if (!strcmp(attr_name, "led"))
         value = usb_send_cmd("GET_LED", -1);
-    else if (!strcmp(attr_name, "ldr"))
-        value = usb_send_cmd("GET_LDR", -1);
     else
-        value = usb_send_cmd("GET_THRESHOLD", -1);
+        value = usb_send_cmd("GET_LDR", -1);
 
-    sprintf(buff, "%d\n", value);                   // Cria a mensagem com o valor do led, ldr ou threshold
+    sprintf(buff, "%d\n", value);                   // Cria a mensagem com o valor do led ou ldr
     return strlen(buff);
 }
 
-// Executado quando o arquivo /sys/kernel/smartlamp/{led, ldr, threshold} é escrito (e.g., echo "100" | sudo tee -a /sys/kernel/smartlamp/led)
+// Executado quando o arquivo /sys/kernel/smartcarbosensor/{led, ldr} é escrito (e.g., echo "100" | sudo tee -a /sys/kernel/smartcarbosensor/led)
 static ssize_t attr_store(struct kobject *sys_obj, struct kobj_attribute *attr, const char *buff, size_t count) {
     long ret, value;
     const char *attr_name = attr->attr.name;
 
     ret = kstrtol(buff, 10, &value);
     if (ret) {
-        printk(KERN_ALERT "SmartLamp: valor de %s invalido.\n", attr_name);
+        printk(KERN_ALERT "SmartCarbo: valor de %s invalido.\n", attr_name);
         return -EACCES;
     }
 
-    printk(KERN_INFO "SmartLamp: Setando %s para %ld ...\n", attr_name, value);
+    printk(KERN_INFO "SmartCarbo: Setando %s para %ld ...\n", attr_name, value);
 
     if (!strcmp(attr_name, "led"))
         ret = usb_send_cmd("SET_LED", value);
-    else if (!strcmp(attr_name, "threshold"))
-        ret = usb_send_cmd("SET_THRESHOLD", value);
     else {
-        printk(KERN_ALERT "SmartLamp: o valor do ldr (sensor de luz) eh apenas para leitura.\n");
+        printk(KERN_ALERT "SmartCarbo: o valor do ldr (sensor de monoxido) eh apenas para leitura.\n");
         return -EACCES;
     }
     if (ret < 0) {
-        printk(KERN_ALERT "SmartLamp: erro ao setar o valor do %s.\n", attr_name);
+        printk(KERN_ALERT "SmartCarbo: erro ao setar o valor do %s.\n", attr_name);
         return -EACCES;
     }
 
