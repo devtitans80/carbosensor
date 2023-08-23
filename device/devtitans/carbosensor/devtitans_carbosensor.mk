05bb3f3065b8c4d3645dbb036aacae246a65319a
devtitans80 <139183464+devtitans80@users.noreply.github.com>
2023-08-23 00:00:34 -0400

    Update devtitans_carbosensor.mk
    add manager

--- device/devtitans/carbosensor/devtitans_carbosensor.mk
+++ device/devtitans/carbosensor/devtitans_carbosensor.mk
@@ -4,40 +4,39 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/sdk_phone_x86_64.mk)
 # Sobrescreve algumas variáveis com os dados do novo produto
 PRODUCT_NAME := devtitans_carbosensor
 PRODUCT_DEVICE := carbosensor
-PRODUCT_BRAND := carboSensorBrand
-PRODUCT_MODEL := carboSensorModel
+PRODUCT_BRAND := carbosensorBrand
+PRODUCT_MODEL := carbosensorModel
 
 # Copia o arquivo devtitans.txt para o /system/etc da imagem do Android
 PRODUCT_COPY_FILES += \
-##	device/devtitans/kraken/devtitans.txt:system/etc/devtitans.txt \
-#	device/devtitans/kraken/kraken.rc:vendor/etc/init/kraken.rc \
+	device/devtitans/carbosensor/carbosensor.rc:vendor/etc/init/carbosensor.rc \
 	device/devtitans/carbosensor/bootanimation.zip:product/media/bootanimation.zip
 
 PRODUCT_SYSTEM_PROPERTIES += \
-	ro.devtitans.name=carboSensor
+    ro.devtitans.name=CarboSensor
 
 PRODUCT_PRODUCT_PROPERTIES += \
-	ro.product.devtitans.version=1.0
+    ro.product.devtitans.version=1.0
 
-#PRODUCT_VENDOR_PROPERTIES += \
-#	ro.vendor.devtitans.hardware=ModelB
-
-#PRODUCT_PRODUCT_PROPERTIES += \
-#	ro.config.wallpaper=product/media/wallpaper/default_wallpaper2.png
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.devtitans.hardware=ModelB
 
 # Seta o diretório de overlays
 PRODUCT_PACKAGE_OVERLAYS = device/devtitans/carbosensor/overlay
 
 PRODUCT_PACKAGES += \
-	UniversalMediaPlayer \
+    UniversalMediaPlayer
 
-#BOARD_SEPOLICY_DIRS += device/devtitans/carbosensor/sepolicy
+# Carbosensor AIDL Interface
+PRODUCT_PACKAGES += devtitans.carbosensor
 
-# Smartlamp AIDL Interface
-PRODUCT_PACKAGES += devtitans.smartcarbo
+# Carbosensor Binder Service
+PRODUCT_PACKAGES += devtitans.carbosensor-service
 
-# Smartlamp Binder Service
-#PRODUCT_PACKAGES += devtitans.smartlamp-service
+# Device Framework Matrix (Declara que o nosso produto Kraken precisa do serviço carbosensor)
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/devtitans/carbosensor/device_framework_matrix.xml
 
-# Device Framework Matrix (Declara que o nosso produto Kraken precisa do serviço smartlamp)
-#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/devtitans/carbosensor/device_framework_matrix.xml
+# Manager
+PRODUCT_PACKAGES += devtitans.carbosensormanager
+    
+BOARD_SEPOLICY_DIRS += device/devtitans/carbosensor/sepolicy
